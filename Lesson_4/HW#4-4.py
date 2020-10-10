06efcb17ab1a5cd165980372de35ca723e2ec72c
]D.A.V[ <teget@mail.ru>
2020-10-10 16:15:45 +0300

    4 in one-string\

--- Lesson_4/HW#4-4.py
+++ Lesson_4/HW#4-4.py
@@ -1,5 +1,4 @@
 from random import randint
-from random import randint
 print(a := [randint(0, 9) for j in range(20)], '\n', [i for i in a if a.count(i) == 1])
 
 '''my_list = [randint(0, 9) for i in range(20)]
